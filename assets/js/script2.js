3fee88a53e058acdbcc17764c273b8f2db90fd01
Henry Aldous <aldous1214@gmail.com>
2021-03-24 21:12:00 -0500

    Rough draft of project from class

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,155 +1,146 @@
 $(document).ready(function () {
-    var currentWeather = $("#current-weather");
-    //from hw
-    var genre1 = ['Action', 'Adeventure', 'Crime', 'Documentary', 'Horor', 'Mystery', 'Thriller', 'War'];
-    var genre2 = ['Animated', 'Comedy', 'Family', 'History', 'Musicals', 'Romance', 'Sci-Fi', 'Western'];
-    var emotions = ['happy', 'sad', 'mindblown', 'flirty', 'scared', 'angry'];
-    var selectedGenre;
-    var apiKey = "6291995da3ff0287da0ec9ee69a1e3a7";
-    var baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
-    var baseUrl2 = 'https://www.omdbapi.com/?apikey=bb99287f&s=';
-    var apiKey2 = 'bb99287f'
-    //for weather icon
-    var iconBaseUrl = "http://openweathermap.org/img/w/";
-    var citySearch = $("#city-search");
-    citySearch.submit(function (event) {
-        event.preventDefault();
-        // this = the form that just submitted!
-        var formValues = $(this).serializeArray();
-        var city = formValues[0].value;
-        console.log(city);
-        searchForCityWeather(city);
-    });
-    function searchForCityWeather(city) {
-        var fullUrl = baseUrl + "q=" + city + "&appid=" + apiKey;
-        console.log(fullUrl);
-        fetch(fullUrl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data);
-                console.log($('#emojis').val());
-                var weather = data.weather;
-                switch (weather[0].icon) {
-                    case 'rainy':
-                    case 'sunny':
-                    case 'cloudy':
-                    case 'snow':
-                    case 'fog':
-                    case 'tornados':
-                        selectedGenre = genre1;
-                        console.log(genre1);
-                        // var genreSelect = $('#genre1');
-                        // for (var i = 0; i < genre1.length; i++) {
-                        //     var option = $('<option>');
-                        //     option.text(genre1[i]);
-                        //     option.val(genre1[i]);
-                        //     genreSelect.append(option);
-                        // }
-                        break;
+  var currentWeather = $("#current-weather");
+  //from hw
+  var genre1 = ['Action', 'Adeventure', 'Crime', 'Documentary', 'Horror', 'Mystery', 'Thriller', 'War'];
+  var genre2 = ['Animation', 'Comedy', 'Family', 'History', 'Music', 'Romance', 'Science Fiction', 'Western'];
+  var emotions = ['happy', 'sad', 'mindblown', 'flirty', 'scared', 'angry'];
+  var selectedGenre;
+  var apiKey = "6291995da3ff0287da0ec9ee69a1e3a7";
+  var baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+  var baseUrl2 = 'https://www.omdbapi.com/?apikey=bb99287f&s=';
+  var apiKey2 = 'bb99287f'
+  //for weather icon
+  var iconBaseUrl = "http://openweathermap.org/img/w/";
+  var citySearch = $("#city-search");
+  citySearch.submit(function (event) {
+      event.preventDefault();
+      // this = the form that just submitted!
+      var formValues = $(this).serializeArray();
+      var city = formValues[0].value;
+      console.log(city);
+      searchForCityWeather(city);
+  });
+  function searchForCityWeather(city) {
+      var fullUrl = baseUrl + "q=" + city + "&appid=" + apiKey;
+      console.log(fullUrl);
+      fetch(fullUrl)
+          .then(function (response) {
+              return response.json();
+          })
+          .then(function (data) {
+              console.log(data);
+              console.log($('#emojis').val());
+              var weather = data.weather;
+              switch (weather[0].icon) {
+                  case 'rainy':
+                  case 'sunny':
+                  case 'cloudy':
+                  case 'snow':
+                  case 'fog':
+                  case 'tornados':
+                      selectedGenre = genre1;
+                      break;
+                  default:
+                      selectedGenre = genre2;
+                      console.log(genre2);
 
-                    default:
-                        selectedGenre = genre2;
-                        console.log(genre2);
+                      break;
+              }
+              console.log(selectedGenre);
+              // var filteredArray;
+              // switch ($('#emojis').val().trim()) {
+              //     case 'happy':
+              //         var happyArray = ['Action', 'Animated'];
+              //         filteredArray = selectedGenre.filter(genre => {
+              //             if (happyArray.includes(genre)) {
+              //                 return genre;
+              //             }
+              //         })
+              //         console.log(filteredArray);
+              //         break;
+              //     case 'sad':
+              //         var sadArray = ['Action', 'Animated'];
+              //         filteredArray = selectedGenre.filter(genre => {
+              //             if (sadArray.includes(genre)) {
+              //                 return genre;
+              //             }
+              //         })
+              //         console.log(filteredArray);
+              //         break;
+              //     case 'happy':
+              //         var happyArray = ['Action', 'Animated'];
+              //         filteredArray = selectedGenre.filter(genre => {
+              //             if (happyArray.includes(genre)) {
+              //                 return genre;
+              //             }
+              //         })
+              //         console.log(filteredArray);
+              //             break;
 
-                        break;
-                }
-                console.log(selectedGenre);
-                var filteredArray;
-                switch ($('#emojis').val().trim()) {
-                    case 'happy':
-                        var happyArray = ['Action', 'Animated'];
-                        filteredArray = selectedGenre.filter(genre => {
-                            if (happyArray.includes(genre)) {
-                                return genre;
-                            }
-                        })
-                        console.log(filteredArray);
-                        break;
-                    case 'sad':
-                        var sadArray = ['Action', 'Animated'];
-                        filteredArray = selectedGenre.filter(genre => {
-                            if (sadArray.includes(genre)) {
-                                return genre;
-                            }
-                        })
-                        console.log(filteredArray);
-                        break;
-                    case 'happy':
-                        var happyArray = ['Action', 'Animated'];
-                        filteredArray = selectedGenre.filter(genre => {
-                            if (happyArray.includes(genre)) {
-                                return genre;
-                            }
-                        })
-                        console.log(filteredArray);
-                            break;
-
-                    default:
-                        break;
-                }
-                var genreSelect = $('#genre');
-                for (var i = 0; i < filteredArray.length; i++) {
-                    var option = $('<option>');
-                    option.text(filteredArray[i]);
-                    option.val (filteredArray[i]);
-                    genreSelect.append(option);
-                }
+              //     default:
+              //         break;
+              // }
+              var genreSelect = $('#genre');
+              for (var i = 0; i < selectedGenre.length; i++) {
+                  var option = $('<option>');
+                  option.text(selectedGenre[i]);
+                  option.val (selectedGenre[i]);
+                  genreSelect.append(option);
+              }
 
 
 
-                var iconUrl = iconBaseUrl + weather[0].icon + ".png";
-                console.log(weather);
-                var weatherDiv = $('<img class="icon-name">');
-                weatherDiv.attr("src", iconUrl);
-                currentWeather.append(weatherDiv);
-                console.log(iconUrl);
-            });
-    }
+              var iconUrl = iconBaseUrl + weather[0].icon + ".png";
+              console.log(weather);
+              var weatherDiv = $('<img class="icon-name">');
+              weatherDiv.attr("src", iconUrl);
+              currentWeather.append(weatherDiv);
+              console.log(iconUrl);
+          });
+  }
 });
 
 
 document.getElementById('search_btn').addEventListener('onClick', function () {
-    var title = document.getElementById('title_text').value;
-    var request = new Request('https://www.omdbapi.com/?apikey=bb99287f&s=' + title);
+  var title = document.getElementById('title_text').value;
+  var request = new Request('https://www.omdbapi.com/?apikey=bb99287f&s=' + title);
 
 
-    fetch(request).then(function (result) {
-        return result.json();
-    })
+  fetch(request).then(function (result) {
+      return result.json();
+  })
 
-        .then(function (data) {
-            var searchEl = document.getElementById('search_result');
-            var len = data.Search.length;
-
-
-            for (var i = 0; i < len; i++) {
-                var movieContainer = document.createElement('div');
-                movieContainer.className = 'search-result--item';
-
-                var titleEl = document.createElement('div');
-                titleEl.innerText = data.Search[i].Title;
-
-                var yearEl = document.createElement('div');
-                yearEl.innerText = data.Search[i].Year;
-
-                var genreEl = document.createElement('div');
-                genreEl.innerText = data.Search[i].Genre;
-
-                var typeEl = document.createElement('div');
-                typeEl.innerText = data.Search[i].Type;
-
-                var posterEl = document.createElement('img');
-                posterEl.src = data.Search[i].Poster;
+      .then(function (data) {
+          var searchEl = document.getElementById('search_result');
+          var len = data.Search.length;
 
 
-                movieContainer.appendChild(posterEl);
-                movieContainer.appendChild(titleEl);
-                movieContainer.appendChild(yearEl);
-                movieContainer.appendChild(typeEl);
-                movieContainer.appendChild(genreEl)
-                searchEl.appendChild(movieContainer);
-            }
-        });
+          for (var i = 0; i < len; i++) {
+              var movieContainer = document.createElement('div');
+              movieContainer.className = 'search-result--item';
+
+              var titleEl = document.createElement('div');
+              titleEl.innerText = data.Search[i].Title;
+
+              var yearEl = document.createElement('div');
+              yearEl.innerText = data.Search[i].Year;
+
+              var genreEl = document.createElement('div');
+              genreEl.innerText = data.Search[i].Genre;
+
+              var typeEl = document.createElement('div');
+              typeEl.innerText = data.Search[i].Type;
+
+              var posterEl = document.createElement('img');
+              posterEl.src = data.Search[i].Poster;
+
+
+              movieContainer.appendChild(posterEl);
+              movieContainer.appendChild(titleEl);
+              movieContainer.appendChild(yearEl);
+              movieContainer.appendChild(typeEl);
+              movieContainer.appendChild(genreEl)
+              searchEl.appendChild(movieContainer);
+          }
+      });
 });
--- assets/omdb.js
+++ assets/omdb.js
@@ -1,40 +1,143 @@
-
-document.getElementById('search_btn').addEventListener('click', function(){
-	var title = document.getElementById('title_text').value;
-	var request = new Request('https://www.omdbapi.com/?apikey=bb99287f&s='+title);
-	
-    
-    fetch(request).then(function(result){
-		return result.json();
-	})
-
-    .then(function(data){
-		var searchEl = document.getElementById('search_result');
-		var len = data.Search.length;
-
-
-		for(var i = 0; i < len; i++){
-			var movieContainer = document.createElement('div');
-			movieContainer.className = 'search-result--item';
-
-			var titleEl = document.createElement('div');
-			titleEl.innerText = data.Search[i].Title;
-
-			var yearEl = document.createElement('div');
-			yearEl.innerText = data.Search[i].Year;
-
-			var typeEl = document.createElement('div');
-			typeEl.innerText = data.Search[i].Type;
-
-			var posterEl = document.createElement('img');
-			posterEl.src = data.Search[i].Poster;
-
-
+document.getElementById("genre").addEventListener("change", function (event) {
+	event.preventDefault();
+	// this keywword = <select>
+	var title = $(this).val();
+	console.log(title);
+	var apiKey = '44f8669609bc7b2064fe2508a8114ac5'; 
+	var url = 
+	  `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&with_genres=`;
+	var genreList = [
+	  {
+		id: 28,
+		name: "Action",
+	  },
+	  {
+		id: 12,
+		name: "Adventure",
+	  },
+	  {
+		id: 16,
+		name: "Animation",
+	  },
+	  {
+		id: 35,
+		name: "Comedy",
+	  },
+	  {
+		id: 80,
+		name: "Crime",
+	  },
+	  {
+		id: 99,
+		name: "Documentary",
+	  },
+	  {
+		id: 18,
+		name: "Drama",
+	  },
+	  {
+		id: 10751,
+		name: "Family",
+	  },
+	  {
+		id: 14,
+		name: "Fantasy",
+	  },
+	  {
+		id: 36,
+		name: "History",
+	  },
+	  {
+		id: 27,
+		name: "Horror",
+	  },
+	  {
+		id: 10402,
+		name: "Music",
+	  },
+	  {
+		id: 9648,
+		name: "Mystery",
+	  },
+	  {
+		id: 10749,
+		name: "Romance",
+	  },
+	  {
+		id: 878,
+		name: "Science Fiction",
+	  },
+	  {
+		id: 10770,
+		name: "TV Movie",
+	  },
+	  {
+		id: 53,
+		name: "Thriller",
+	  },
+	  {
+		id: 10752,
+		name: "War",
+	  },
+	  {
+		id: 37,
+		name: "Western",
+	  },
+	];
+	var selectedGenre = genreList.filter((genre) => {
+	  return genre.name === title;
+	});
+	console.log(selectedGenre);
+	if (selectedGenre.length > 0) {
+	  url = url += selectedGenre[0].id;
+	  fetch(url)
+		.then(function (result) {
+		  return result.json();
+		})
+  
+		.then(function (data) {
+		  var searchEl = document.getElementById("search_result");
+		  console.log(data);
+		  var len = data.results.length;
+  
+		  for (var i = 0; i < len; i++) {
+			var currentMovie = data.results[i];
+		  // adult: false
+		  // backdrop_path: "/9NlswPRK5Kap6KVr59Feah9EBLP.jpg"
+		  // genre_ids: (3) [36, 28, 12]
+		  // id: 631939
+		  // original_language: "en"
+		  // original_title: "Legionnaire's Trail"
+		  // overview: "Noreno, a half-Roman, is entrusted with the mission of crossing the snowy mountains of Armenia, swarming with Parthian patrols, to seek help for his slowly dying men."
+		  // popularity: 345.522
+		  // poster_path: "/6ssoBXQOxNhrsGJoM6Tcvm57V79.jpg"
+		  // release_date: "2020-05-08"
+		  // title: "Legionnaire's Trail"
+		  // video: false
+		  // vote_average: 6.6
+		  // vote_count: 59
+			var movieContainer = document.createElement("div");
+			movieContainer.className = "search-result--item";
+  
+			var titleEl = document.createElement("div");
+			titleEl.innerText = currentMovie.original_title;
+  
+			// Moment JS .format()
+			var yearEl = document.createElement("div");
+			yearEl.innerText = currentMovie.release_date;
+  
+			var typeEl = document.createElement("div");
+			typeEl.innerText = selectedGenre[0].name;
+  
+			var posterEl = document.createElement("img");
+			posterEl.src = `https://image.tmdb.org/t/p/original${currentMovie.backdrop_path}`;
+  
 			movieContainer.appendChild(posterEl);
 			movieContainer.appendChild(titleEl);
 			movieContainer.appendChild(yearEl);
 			movieContainer.appendChild(typeEl);
 			searchEl.appendChild(movieContainer);
-		}
-	});
-});
+		  }
+		});
+	}
+  });
