59e15979f0ea64ce04a54aeb11b77eb2a8211ddd
ksdevinney <katie@devinney.org>
2021-03-24 21:11:58 -0500

    adjusting html and styling

--- assets/omdb.js
+++ assets/omdb.js
@@ -9,6 +9,7 @@ document.getElementById('search_btn').addEventListener('click', function(){
 	})
 
     .then(function(data){
+		console.log(data);
 		var searchEl = document.getElementById('search_result');
 		var len = data.Search.length;
 
@@ -29,11 +30,27 @@ document.getElementById('search_btn').addEventListener('click', function(){
 			var posterEl = document.createElement('img');
 			posterEl.src = data.Search[i].Poster;
 
-
+			// possible alternative?
+			// var emojis = document.createElement('div');
+			// emojis.innerHTML = `<label for="mood">How is your mood?</label>
+			// <select id="emojis" name="emojis">
+			// 	<option value="happy"> &#128512; </option>
+			// 	<option value="sad"> &#128546; </option>
+			// 	<option value="mindblown"> &#129327;</option>
+			// 	<option value="flirty"> &#128536; </option>
+			// 	<option value="scared"> &#128552; </option>
+			// 	<option value="angry"> &#128545; </option>
+			// </select>
+			// <button class="savemood" data-title="${data.Search[i].Title}" >Save Mood for this movie</button>`
+			// var weather = document.createElement("div")
+			// weather.innerHTML = `
+			// <button data-value="icon" ><img src=''/></button>
+			// `
 			movieContainer.appendChild(posterEl);
 			movieContainer.appendChild(titleEl);
 			movieContainer.appendChild(yearEl);
 			movieContainer.appendChild(typeEl);
+			// movieContainer.appendChild(emojis);
 			searchEl.appendChild(movieContainer);
 		}
 	});
